280765e8fcab70c546e8be90e937e8fa0c3fa107
samgongora26 <saul.santiago2426@gmail.com>
2021-08-26 00:24:45 -0500

    comenzando con la logica de negocios

--- js/app.js
+++ js/app.js
@@ -8,7 +8,30 @@ async function enviar_datos(url, datos) {
     }
 }
 
-export async function existe_cuenta() {
+//variables del dom
+const form_movie = document.querySelector("#form_movie_search");
+form_movie.addEventListener("submit", busca_pelicula);
+
+async function busca_pelicula(e) {
+    e.preventDefault();
+    let movie = document.querySelector('#movie').value;
+    console.log("pelicula" + movie);
+    /*
+    var searchElement = 'https://www.omdbapi.com/?s='+movie+'&apikey=thewdb';
+    var movieDetails = [];
+    const url = "../../inc/peticiones/admin/funciones.php";
+    const datos = new FormData();
+    datos.append("accion", "verifica_cuenta");
+
+    
+    const res = await enviar_datos(url, datos);
+    //console.log(res);
+    res.length != 0 ? llenado_contenedor_html(contenido_promociones,res) : ninguna_promocion(); 
+    return cuenta;
+    */
+}
+
+async function guardar_datos() {
     var searchElement = 'https://www.omdbapi.com/?s='+req.query.movie+'&apikey=thewdb';
     var movieDetails = [];
     const url = "../../inc/peticiones/admin/funciones.php";
@@ -19,4 +42,4 @@ export async function existe_cuenta() {
     //console.log(res);
     const cuenta = res.cuenta_existente;
     return cuenta;
-  }
+}
