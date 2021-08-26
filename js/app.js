d8f1ed18449d7c9f91b477df6ba4e89099fc3033
samgongora26 <saul.santiago2426@gmail.com>
2021-08-26 02:04:42 -0500

    ver pelicula en especifico

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+//variables del dom
+const btn_search_movie = document.querySelector("#btn_search_movie");
+const movies_list = document.querySelector("#movies_list");
+btn_search_movie.addEventListener("click", busca_peliculas);
+const movie_txt = document.querySelector('#movie');
+
 async function enviar_datos(url) {
     try {
       const res = await fetch(url, { method: "POST", body: "" });
@@ -8,17 +14,12 @@ async function enviar_datos(url) {
     }
 }
 
-//variables del dom
-const btn_search_movie = document.querySelector("#btn_search_movie");
-const movies_list = document.querySelector("#movies_list");
-btn_search_movie.addEventListener("click", busca_pelicula);
-
-async function busca_pelicula(e) {
+async function busca_peliculas(e) {
     e.preventDefault();
     movies_list.innerHTML="";
-    let movie = document.querySelector('#movie').value;
-    console.log("pelicula" + movie);
-    const url = 'https://www.omdbapi.com/?s='+movie+'&apikey=thewdb';
+    let movies = movie_txt.value;
+    console.log("pelicula " + movies);
+    const url = 'https://www.omdbapi.com/?s='+movies+'&apikey=thewdb';
     const res = await enviar_datos(url);
     console.log(res);
     res.length != 0 ? llenado_contenedor_html(res) : alert("no se ha encontrado tu pelicula :("); 
@@ -37,10 +38,41 @@ async function llenado_contenedor_html(res){
                     <small>${Year}</small>
                     <p>${Type}</p>
                     <br>
+                    <button type="button" onclick="busca_pelicula('${Title}');" class="buy-now">Elegir pelicula<i class="fa fa-check"></i></button>
                 </a>
             </li>
                     `;
     });
+}
+
+async function busca_pelicula(movie) {
+    movies_list.innerHTML="";
+    console.log("pelicula" + movie);
+    const url = 'https://www.omdbapi.com/?t='+movie+'&apikey=thewdb';
+    const res = await enviar_datos(url);
+    console.log(res);
+    res.length != 0 ? llenado_contenedor_html_especifico(res) : alert("no se ha encontrado tu pelicula :("); 
+}
+
+async function llenado_contenedor_html_especifico(res){
+    movies_list.innerHTML += `
+            <li class="card">
+                <a class="card-image">
+                    <img src="${res.Poster}"/>
+                </a>
+                <a class="card-description">
+                    <h2>${res.Title}</h2>
+                    <small>${res.Year}</small> <small>${res.Released}</small>
+                    <p>${res.Type}</p> <small> ${res.Genre} </small>
+                    <p>${res.Rated}</p>
+                    <p>Director: ${res.Director}</p>
+                    <small>${res.Plot}</small>
+                    <br>
+                    <br>
+                    <button onclick="elegir_pelicula(${res.Title})"; class="buy-now">Elegir pelicula<i class="fa fa-check"></i></button>
+                </a>
+            </li>
+                    `;
 }
 
 async function guardar_datos() {
@@ -55,3 +87,4 @@ async function guardar_datos() {
     const cuenta = res.cuenta_existente;
     return cuenta;
 }
+
