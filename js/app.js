e244079c6b122e9ca8d86ddc2e7240d724084971
samgongora26 <saul.santiago2426@gmail.com>
2021-08-26 01:16:36 -0500

    consumo de la api de peliculas

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-async function enviar_datos(url, datos) {
+async function enviar_datos(url) {
     try {
-      const res = await fetch(url, { method: "POST", body: datos });
+      const res = await fetch(url, { method: "POST", body: "" });
       const data = await res.json();
       return data;
     } catch (error) {
@@ -9,26 +9,38 @@ async function enviar_datos(url, datos) {
 }
 
 //variables del dom
-const form_movie = document.querySelector("#form_movie_search");
-form_movie.addEventListener("submit", busca_pelicula);
+const btn_search_movie = document.querySelector("#btn_search_movie");
+const movies_list = document.querySelector("#movies_list");
+btn_search_movie.addEventListener("click", busca_pelicula);
 
 async function busca_pelicula(e) {
     e.preventDefault();
+    movies_list.innerHTML="";
     let movie = document.querySelector('#movie').value;
     console.log("pelicula" + movie);
-    /*
-    var searchElement = 'https://www.omdbapi.com/?s='+movie+'&apikey=thewdb';
-    var movieDetails = [];
-    const url = "../../inc/peticiones/admin/funciones.php";
-    const datos = new FormData();
-    datos.append("accion", "verifica_cuenta");
+    const url = 'https://www.omdbapi.com/?s='+movie+'&apikey=thewdb';
+    const res = await enviar_datos(url);
+    console.log(res);
+    res.length != 0 ? llenado_contenedor_html(res) : alert("no se ha encontrado tu pelicula :("); 
+}
 
-    
-    const res = await enviar_datos(url, datos);
-    //console.log(res);
-    res.length != 0 ? llenado_contenedor_html(contenido_promociones,res) : ninguna_promocion(); 
-    return cuenta;
-    */
+async function llenado_contenedor_html(res){
+    res.Search.forEach((peli) => {
+        const { Title, Year, Type, Poster } = peli;
+        movies_list.innerHTML += `
+            <li class="card">
+                <a class="card-image">
+                    <img src="${Poster}"/>
+                </a>
+                <a class="card-description">
+                    <h2>${Title}</h2>
+                    <small>${Year}</small>
+                    <p>${Type}</p>
+                    <br>
+                </a>
+            </li>
+                    `;
+    });
 }
 
 async function guardar_datos() {
